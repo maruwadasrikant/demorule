57082976e4f44c037ef150a39db370870091e9c4
maruwadasrikant <srikantmaruwada@gmail.com>
2020-06-04 21:38:03 +0530

    Added 2nd rule

---
+++ MyOrderProcessApp/Demo.RuleProcessing/Products/MemberShip.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Demo.RuleProcessing.Products
+{
+    public class MemberShip : IDomainEntity
+    {
+        public string Name { get; set; } = "MememberShip";
+
+        public string User { get; set; }
+    }
+}
---
+++ MyOrderProcessApp/Demo.RuleProcessing/Rules/MemberShipUpgrade.cs
@@ -0,0 +1,19 @@
+﻿using Demo.RuleProcessing.Products;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Demo.RuleProcessing.Rules
+{
+    public class MemberShipUpgrade : BaseRule
+    {
+        public MemberShipUpgrade(IDomainEntity domainEntity) : base(domainEntity)
+        {
+        }
+
+        public override string ExecuteRule()
+        {
+            return $"Upgrade {base.ProductName}";
+        }
+    }
+}
